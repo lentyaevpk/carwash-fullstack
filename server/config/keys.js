6b66dd52bbe2dc03d24224cb5ed1ef4756c9ec73
Lentyaev Pavel <pavlent3@gmail.com>
2020-04-08 18:13:20 +0300

    in progress...

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://pavel:qwe123@cluster0-z5iag.mongodb.net/',
+    secret: 'yoursecret'
+}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log(`Database connected succsesfully ${db}`)
+}).catch(err => {
+    console.log(`Unable to connect with the database ${err}`)
+});
+
+const users = require('./routes/api/users');
+
+app.use('/api/users', users);
 
 const posts = require('./routes/api/posts');
 

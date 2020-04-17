799fd1df70c2777e2228658fa0e447730b5458b6
Lentyaev Pavel <pavlent3@gmail.com>
2020-04-17 21:52:26 +0300

    commit

--- client/src/components/Address.vue
+++ client/src/components/Address.vue
@@ -2,15 +2,6 @@
   <div class="address">
     <h2 class="title address__title">Мы находимся по адресу:</h2>
     <h4 class="address__subtitle subtitle">г.Мичуринск, ул. Братьев Сорокиных 3</h4>
-    <!-- <iframe
-      class="map"
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2559.9880268732495!2d45.37885099479314!3d50.08651103476828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x411173d16936f29d%3A0x9f02e20046edf9fa!2z0JDQktCi0J7QnNCe0JnQmtCQINCd0JAg0J_QkNCg0KLQmNCX0JDQndCh0JrQntCZ!5e0!3m2!1sru!2sru!4v1564319372739!5m2!1sru!2sru"
-      width="800"
-      height="600"
-      frameborder="0"
-      style="border:0"
-      allowfullscreen
-    ></iframe> -->
   </div>
 </template>
 
--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(passport.initialize());
 
 require('./config/passport')(passport);

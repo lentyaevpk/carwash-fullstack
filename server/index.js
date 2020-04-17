73d7c8b2ab4f7ea12525b54340bb1c5383d4d84c
Lentyaev Pavel <pavlent3@gmail.com>
2020-04-17 21:56:32 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
